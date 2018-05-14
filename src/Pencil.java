d739822857c845673d7db76080918741c29bd06d
arad13 <35691630+arad13@users.noreply.github.com>
2018-05-14 08:47:14 -0400

    Create a pencil with durability and retrieve that durability.

--- src/Pencil.java
+++ src/Pencil.java
@@ -1,6 +1,15 @@
 class Pencil {
+    private int durability;
+
+    Pencil(int newPencilDurability){
+        durability = newPencilDurability;
+    }
 
     void Write(Paper paper, String textToWrite){
         paper.setText(paper.getText() + textToWrite);
+    }
+
+    int getDurability(){
+        return durability;
     }
 }
--- tests/PencilTests.java
+++ tests/PencilTests.java
@@ -6,8 +6,14 @@ public class PencilTests {
     @Test
     public void givenAPaperWithInitialTextWriteAdditionalTextOnThePaper() {
         Paper paper = new Paper("This is some initial text.");
-        Pencil pencil = new Pencil();
+        Pencil pencil = new Pencil(15);
         pencil.Write(paper, "  Here is some followup text.");
         assertEquals("This is some initial text.  Here is some followup text.", paper.getText());
+    }
+
+    @Test
+    public void createAPencilWithADurabilityAndRetrieveThatDurability(){
+        Pencil pencil = new Pencil(15);
+        assertEquals(15, pencil.getDurability());
     }
 }
