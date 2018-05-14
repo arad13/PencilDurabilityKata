a0bafcf4cd64bcf0794d7b418a5d2d283a6f42b1
arad13 <35691630+arad13@users.noreply.github.com>
2018-05-14 08:47:59 -0400

    Refactor PencilTests class

--- tests/PencilTests.java
+++ tests/PencilTests.java
@@ -2,18 +2,17 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class PencilTests {
+    private Pencil pencil = new Pencil(15);
+    private Paper paper = new Paper("This is some initial text.");
 
     @Test
     public void givenAPaperWithInitialTextWriteAdditionalTextOnThePaper() {
-        Paper paper = new Paper("This is some initial text.");
-        Pencil pencil = new Pencil(15);
         pencil.Write(paper, "  Here is some followup text.");
         assertEquals("This is some initial text.  Here is some followup text.", paper.getText());
     }
 
     @Test
     public void createAPencilWithADurabilityAndRetrieveThatDurability(){
-        Pencil pencil = new Pencil(15);
         assertEquals(15, pencil.getDurability());
     }
 }
