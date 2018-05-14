87cd76ab6e89c536185e7c4246b6f5543cf21318
arad13 <35691630+arad13@users.noreply.github.com>
2018-05-14 08:40:59 -0400

    Refactor PaperTests class

--- tests/PaperTests.java
+++ tests/PaperTests.java
@@ -3,16 +3,15 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class PaperTests {
+    private Paper paper = new Paper("This is an initial paper string.");
 
     @Test
     public void givenAnInitialTextForPaperGetTheTextBackAsOutput() {
-        Paper paper = new Paper("This is an initial paper string.");
         assertEquals("This is an initial paper string.", paper.getText());
     }
 
     @Test
     public void givenAnInitialTextForPaperSetTheTextToSomethingDifferent(){
-        Paper paper = new Paper("This is an initial paper string.");
         paper.setText("This is a modified paper string.");
         assertEquals("This is a modified paper string.", paper.getText());
     }
