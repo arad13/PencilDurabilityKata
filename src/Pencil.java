1f1241ec5d7a5f3973c30cfad91d45e587d6f82a
arad13 <35691630+arad13@users.noreply.github.com>
2018-05-14 11:36:44 -0400

    Edit in text of longer length than the available whitespace.

--- src/Pencil.java
+++ src/Pencil.java
@@ -3,6 +3,7 @@ class Pencil {
     private final int initialDurability;
     private int length;
     private int eraserDurability;
+    private int lastErasedTextIndex;
 
     Pencil(int newPencilDurability, int newPencilLength, int newPencilEraserDurability){
         initialDurability = durability = newPencilDurability;
@@ -50,13 +51,38 @@ class Pencil {
         int numberOfCharactersThatCannotBeErased = 0;
         int lengthOfCharactersToBeErased = textToErase.length();
 
+        if( lastIndex < 0 )
+            return;
+
         if( eraserDurability < textToErase.length()){
             numberOfCharactersThatCannotBeErased = textToErase.length() - eraserDurability;
             lengthOfCharactersToBeErased = eraserDurability;
         }
         paperText.replace(lastIndex + numberOfCharactersThatCannotBeErased, lastIndex + numberOfCharactersThatCannotBeErased + lengthOfCharactersToBeErased, createSpaceString(lengthOfCharactersToBeErased));
 
+        lastErasedTextIndex = lastIndex + numberOfCharactersThatCannotBeErased;
+
         eraserDurability -= lengthOfCharactersToBeErased;
+
+        paper.setText(paperText.toString());
+    }
+
+    void Edit(Paper paper, String textToEditIn){
+        StringBuilder paperText = new StringBuilder(paper.getText());
+
+        for( int i = 0; i < textToEditIn.length(); i++ ){
+            char characterToInsert = textToEditIn.charAt(i);
+
+            if( Character.isLowerCase(characterToInsert) )
+                durability --;
+            else if( Character.isUpperCase(characterToInsert))
+                durability -= 2;
+
+            if( lastErasedTextIndex + i >= paperText.length() )
+                paperText.append(characterToInsert);
+            else
+                paperText.replace(lastErasedTextIndex+i, lastErasedTextIndex+i+1, tryAndReplaceCharacter(paperText.charAt(lastErasedTextIndex+i), characterToInsert));
+        }
 
         paper.setText(paperText.toString());
     }
@@ -67,5 +93,14 @@ class Pencil {
             strReturn.append(" ");
         }
         return strReturn.toString();
+    }
+
+    private String tryAndReplaceCharacter(char existingCharToReplace, char newCharBeingInserted){
+        if( existingCharToReplace != ' ' && newCharBeingInserted != ' ' && newCharBeingInserted != existingCharToReplace )
+            return "@";
+        else if( existingCharToReplace == ' ' )
+            return Character.toString(newCharBeingInserted);
+        else
+            return Character.toString(existingCharToReplace);
     }
 }
